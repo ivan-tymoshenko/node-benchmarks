bf58d7fc4df9886e3f09214664a94dab427950b1
Travis CI <travis@travis-ci.org>
2018-04-01 17:11:07 +0300

    v0.2.0

--- mhpt.js
+++ mhpt.js
@@ -4,40 +4,43 @@ const fs = require('fs');
 const path = require('path');
 const { fork, execSync } = require('child_process');
 const makeResults = require('./lib/create-results.js');
-const PATH_TO_GET_TIME = path.join(__dirname, '/lib/get-time.js');
-const PATH_TO_GET_NODE_PATH = path.join(__dirname, '/lib/get-node-path.sh');
+
+const paths = {
+  GET_TIME: path.join(__dirname, '/lib/get-time.js'),
+  GET_NODE_PATH: path.join(__dirname, '/lib/get-node-path.sh')
+};
 
 const defaultParameters = {
-  count: 25000,
-  versions: [process.versions.node],
-  MAX_ANOMALY_PERCENT: 5,
-  NUMBER_OF_STEPS: 25
+  COUNT: 25000,
+  NODE_VERSIONS: [process.versions.node],
+  MAX_ANOMALY_PERCENT: 5
 };
+
 
 const getNodePath = (
   // Function which gets path to node by version
   version // String, versions of node, example: 9.4.0, 8.0, 7
   // Returns: String, path to node
 ) => {
-  let pathToNode = null;
-  const command = ['sh', PATH_TO_GET_NODE_PATH, version].join(' ');
+  let NODE_PATH = null;
+  const command = ['sh', paths.GET_NODE_PATH, version].join(' ');
   if (process.env.NVM_DIR) {
-    pathToNode = execSync(command + ' nvm').toString().trim();
-    if (fs.existsSync(pathToNode)) return pathToNode;
+    NODE_PATH = execSync(command + ' nvm').toString().trim();
+    if (fs.existsSync(NODE_PATH)) return NODE_PATH;
   }
   if (process.env.NVS_HOME) {
-    pathToNode = execSync(command + ' nvs').toString().trim();
-    if (fs.existsSync(pathToNode)) return pathToNode;
+    NODE_PATH = execSync(command + ' nvs').toString().trim();
+    if (fs.existsSync(NODE_PATH)) return NODE_PATH;
   }
   throw new Error('Node: ' + version + ' not found');
 };
 
 const getFullVersion = (
   // Function which gets full node version from node path
-  pathToNode // String, path to node
+  NODE_PATH // String, path to node
   // Returns: String, full node version
 ) => {
-  const path = pathToNode.split('/');
+  const path = NODE_PATH.split('/');
   const version = path[path.length - 3];
   return version[0] === 'v' ? version.substring(1, version.length) : version;
 };
@@ -52,9 +55,9 @@ const prepareRequests = (
   const requests = [];
   versions.forEach(version => {
     const versionResults = []; // results of all functions in one node version
-    const nodePath = getNodePath(version);
-    const fullVersion = getFullVersion(nodePath);
-    const request = [fullVersion, versionResults, nodePath];
+    const NODE_PATH = getNodePath(version);
+    const fullVersion = getFullVersion(NODE_PATH);
+    const request = [fullVersion, versionResults, NODE_PATH];
     testsSync.forEach(func => requests.push([func, 'sync'].concat(request)));
     testsAsync.forEach(func => requests.push([func, 'async'].concat(request)));
   });
@@ -84,9 +87,9 @@ const speed = (
     else value.forEach(func => asyncFunctions.push(func));
   });
 
-  if (process.argv[1] === PATH_TO_GET_TIME) { // start only from child process
+  if (process.argv[1] === paths.GET_TIME) { // start only from child process
     let modRef = module;
-    while (modRef.parent.filename !== PATH_TO_GET_TIME) modRef = modRef.parent;
+    while (modRef.parent.filename !== paths.GET_TIME) modRef = modRef.parent;
     syncFunctions.forEach(func => { modRef.exports[func.name] = func; });
     asyncFunctions.forEach(func => { modRef.exports[func.name] = func; });
     return;
@@ -116,19 +119,19 @@ const speed = (
     versionResults, // Array, results of all functions in one node version
     nodePath // String, path to the needed version of the node
   ) => {
-    const forked = fork(PATH_TO_GET_TIME, { execPath: nodePath });
+    const forked = fork(paths.GET_TIME, { execPath: nodePath });
     const count = parameters.count;
     forked.send({
       name: func.name, count, type, path,
-      MAX_ANOMALY_PERCENT: parameters.MAX_ANOMALY_PERCENT,
-      NUMBER_OF_STEPS: parameters.NUMBER_OF_STEPS
+      percent: parameters.MAX_ANOMALY_PERCENT
     });
     forked.on('message', result => {
       versionResults.push(result);
       if (versionResults.length === numberOfFuncs) {
         results.set(version, versionResults);
         if (results.size === parameters.versions.length) {
-          makeResults(results, caption, count, parameters.NUMBER_OF_STEPS);
+          makeResults(results, caption, count);
+
         }
       }
       if (requests.length) sendRequest(...requests.pop());
--- test/test-sync.js
+++ test/test-sync.js
@@ -99,7 +99,7 @@ mhtp.speed('Benchmark example', [
   objectCreate,
   callFactory
 ], {
-  count: 2000019,
+  count: 2000000,
   MAX_ANOMALY_PERCENT: 5,
-  versions: ['9', '8', '7']
+  versions: ['9.8']
 });
